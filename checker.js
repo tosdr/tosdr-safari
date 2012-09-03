69e1b5939767bf4f01c08787d1665c914842f54e
martin <martin@siarp.de>
2012-09-03 22:37:34 +0200

    initial
    icons show up in button, button opens window with tos-dr.info

---
+++ checker.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function getTosButton (tab) {
+  var toolbarItems = safari.extension.toolbarItems
+  for (var toolbarItemsCount = 0, toolbarItemsLength = toolbarItems.length; toolbarItemsCount < toolbarItemsLength; ++toolbarItemsCount) {
+    var toolbarItem = toolbarItems[toolbarItemsCount]
+      if (toolbarItem.identifier == "tos button") {
+      var tabs = toolbarItem.browserWindow.tabs
+      for (var tabsCount = 0, tabsLength = tabs.length; tabsCount < tabsLength; ++tabsCount) {
+        var _tab = tabs[tabsCount]
+        if (_tab == tab) {
+          return toolbarItem
+        }
+      }
+    }
+  }
+}
+
+safari.application.addEventListener('navigate', function (event) {
+  var service = Tosdr.getService(event.target.url)
+  var button = getTosButton(event.target)
+  if (service && service.tosdr.rated) {
+    button.image = safari.extension.baseURI + service.tosdr.rated.toLowerCase() + '.png'
+  }
+  else {
+    button.image = safari.extension.baseURI + 'false.png'
+  }
+})
+
+safari.application.addEventListener("command", function (event) {
+  if (event.command == 'open tosdr site') {
+    safari.application.openBrowserWindow().activeTab.url = 'http://tos-dr.info'
+  }
+}, false)
